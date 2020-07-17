05225ac8163090d9a051dad5583f37bf3b7e75de
AahzBrut <partyomenko@gmail.com>
2020-07-17 18:51:14 +0300

    Add layout for CreateHabitActivity

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -9,3 +9,15 @@ c
 3app/src/main/java/com/example/habittrainer/Habit.kt,0\0\0071f0eb1e6bc3749ab22a22d61f4cc8f5a3ba7a
 k
 ;app/src/main/java/com/example/habittrainer/HabitsAdapter.kt,c\9\c9bcf263195203300474fbe220d00a15a49ea2f1
+?
+settings.gradle,0\5\05efc8b1657769a27696d478ded1e95f38737233
+@
+local.properties,0\7\0712df971a99ac4d2fccb8e0fb19f377f3374cca
+A
+gradle.properties,2\a\2afbb999f001938c88fa43fc2ef52abf0f8213e4
+F
+app/proguard-rules.pro,9\e\9e08934d811afe28fbc77aaa3c0d747b94348db9
+X
+(gradle/wrapper/gradle-wrapper.properties,f\b\fbe448ebfc3eb2d4e308f6b8b043666f5b57235e
+q
+Aapp/src/main/java/com/example/habittrainer/CreateHabitActivity.kt,c\b\cb6fe4fe9754b82b30fe3a655e21f7d8b0fb1af4
--- app/src/main/java/com/example/habittrainer/MainActivity.kt
+++ app/src/main/java/com/example/habittrainer/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.habittrainer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -21,5 +23,19 @@ class MainActivity : AppCompatActivity() {
 
         menuInflater.inflate(R.menu.main_menu, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.add_habit) {
+            switchToActivity(CreateHabitActivity::class.java)
+        }
+
+        return true
+    }
+
+    private fun switchToActivity(clazz: Class<*>) {
+        val intent = Intent(this, clazz)
+        startActivity(intent)
     }
 }
