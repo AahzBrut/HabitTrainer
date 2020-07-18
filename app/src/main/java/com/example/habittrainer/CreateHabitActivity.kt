8ebda820bf258d7ef152625a2b489ef3744d0431
AahzBrut <partyomenko@gmail.com>
2020-07-18 12:47:51 +0300

    Add choose image intent and activity

--- app/src/main/java/com/example/habittrainer/CreateHabitActivity.kt
+++ app/src/main/java/com/example/habittrainer/CreateHabitActivity.kt
@@ -1,11 +1,28 @@
 package com.example.habittrainer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class CreateHabitActivity : AppCompatActivity() {
+
+    private val tag = CreateHabitActivity::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_habit)
+    }
+
+    fun chooseImage(view: View) {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+
+        val chooser = Intent.createChooser(intent, "Choose image for habit")
+        startActivity(chooser)
+
+        Log.d(tag, "Intent to choose image sent...")
     }
 }
