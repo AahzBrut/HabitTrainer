d42536b859905cdc7152cf1077701eee8b40f546
AahzBrut <partyomenko@gmail.com>
2020-07-18 14:56:28 +0300

    Add save habit

--- app/src/main/java/com/example/habittrainer/CreateHabitActivity.kt
+++ app/src/main/java/com/example/habittrainer/CreateHabitActivity.kt
@@ -1,14 +1,25 @@
 package com.example.habittrainer
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.annotation.RequiresApi
+import androidx.core.net.toFile
+import kotlinx.android.synthetic.main.activity_create_habit.*
+import java.io.IOException
 
 class CreateHabitActivity : AppCompatActivity() {
 
-    private val tag = CreateHabitActivity::class.java.simpleName
+    private val TAG = CreateHabitActivity::class.java.simpleName
+    private val CHOOSE_IMAGE_REQUEST = 1
+    private var imageBitmap: Bitmap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +32,57 @@ class CreateHabitActivity : AppCompatActivity() {
         intent.action = Intent.ACTION_GET_CONTENT
 
         val chooser = Intent.createChooser(intent, "Choose image for habit")
-        startActivity(chooser)
+        startActivityForResult(chooser, CHOOSE_IMAGE_REQUEST)
 
-        Log.d(tag, "Intent to choose image sent...")
+        Log.d(TAG, "Intent to choose image sent...")
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == CHOOSE_IMAGE_REQUEST && resultCode == Activity.RESULT_OK &&
+                data != null && data.data != null) {
+
+            Log.d(TAG, "An image was chosen by the user.")
+
+            val bitmap = tryReadBitmap(data.data!!)
+            bitmap?.let {
+                iv_image.setImageBitmap(it)
+                imageBitmap = it
+
+                Log.d(TAG, "Image view updated with selected image.")
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    private fun tryReadBitmap(data: Uri): Bitmap? {
+        return try {
+            val source = ImageDecoder.createSource(data.toFile())
+            ImageDecoder.decodeBitmap(source)
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+            null
+        }
+    }
+
+    fun storeHabit(view: View) {
+        if (et_title.text.toString().isBlank() || et_description.text.toString().isBlank()) {
+
+            Log.d(TAG, "No habit stored: title and/or description are blank.")
+            displayErrorMessage("Your habit needs engaging title and description.")
+            return
+        } else if (imageBitmap == null) {
+
+            Log.d(TAG, "No habit stored: image not selected")
+            displayErrorMessage("Your habit needs a motivating picture.")
+            return
+        }
+    }
+
+    private fun displayErrorMessage(msg: String) {
+        tv_error.text = msg
+        tv_error.visibility = View.VISIBLE
     }
 }
